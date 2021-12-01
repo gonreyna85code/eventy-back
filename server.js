932720f48db3250e5ea985d3f310681a91819c70
gonzalo <gonreyna85@gmail.com>
2021-12-01 18:32:32 -0300

    make it better

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
+require('dotenv').config();
 const mongoose = require("mongoose");
 const express = require("express");
-//const cors = require("cors");
+const cors = require("cors");
 const passport = require("passport");
 const passportLocal = require("passport-local").Strategy;
 const cookieParser = require("cookie-parser");
@@ -9,7 +10,7 @@ const bodyParser = require("body-parser");
 const app = express();
 const user = require("./routes/user");
 const event = require("./routes/event");
-//hola
+
 
 mongoose.connect(
   "mongodb+srv://gonreyna85:gonreyna85@cluster0.bubyh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
@@ -21,6 +22,13 @@ mongoose.connect(
     console.log("Mongoose Is Connected");
   }
 );
+server.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+});
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -39,7 +47,9 @@ require("./passportConfig")(passport);
 
 app.use('/', user)
 app.use('/', event)
-
+app.use(
+  cors()
+);
 const PORT = process.env.PORT || 4000;
 
 
