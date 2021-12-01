1ba7ce19a848846b6191aafcea0815351fbf6daa
gonzalo <gonreyna85@gmail.com>
2021-12-01 19:36:05 -0300

    make it better

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require("express");
 //const cors = require("cors");
 //const passport = require("passport");
 //const passportLocal = require("passport-local").Strategy;
-const cookieParser = require("cookie-parser");
+//const cookieParser = require("cookie-parser");
 //const session = require("express-session");
 const bodyParser = require("body-parser");
 const app = express();
@@ -23,11 +23,11 @@ mongoose.connect(
   }
 );
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+//app.use(bodyParser.urlencoded({ extended: true }));
 //app.use(cors(  ));
 
 
-app.use(cookieParser("secretcode"));
+//app.use(cookieParser("secretcode"));
 //app.use(passport.initialize());
 //app.use(passport.session());
 //require("./passportConfig")(passport);
