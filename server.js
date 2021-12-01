1574cb78efc1c9885a57b63ed1096ab76b0f3725
gonzalo <gonreyna85@gmail.com>
2021-12-01 18:35:05 -0300

    make it better

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ mongoose.connect(
     console.log("Mongoose Is Connected");
   }
 );
-server.use((req, res, next) => {
+app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
