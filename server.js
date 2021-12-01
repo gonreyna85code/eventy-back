6a0c1d8dbfb242ff259e2d6fdc1953b77506133e
gonzalo <gonreyna85@gmail.com>
2021-12-01 19:40:47 -0300

    make it better

--- server.js
+++ server.js
@@ -1,37 +1,45 @@
 require('dotenv').config();
-const mongoose = require("mongoose");
+//const mongoose = require("mongoose");
 const express = require("express");
-//const cors = require("cors");
-//const passport = require("passport");
-//const passportLocal = require("passport-local").Strategy;
-//const cookieParser = require("cookie-parser");
-//const session = require("express-session");
+const cors = require("cors");
+const passport = require("passport");
+const passportLocal = require("passport-local").Strategy;
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
 const bodyParser = require("body-parser");
 const app = express();
 const user = require("./routes/user");
 const event = require("./routes/event");
 
 
-mongoose.connect(
-  "mongodb+srv://gonreyna85:gonreyna85@cluster0.bubyh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  },
-  () => {
-    console.log("Mongoose Is Connected");
-  }
-);
+// mongoose.connect(
+//   "mongodb+srv://gonreyna85:gonreyna85@cluster0.bubyh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+//   {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   },
+//   () => {
+//     console.log("Mongoose Is Connected");
+//   }
+// );
 app.use(bodyParser.json());
-//app.use(bodyParser.urlencoded({ extended: true }));
-//app.use(cors(  ));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 
-//app.use(cookieParser("secretcode"));
-//app.use(passport.initialize());
-//app.use(passport.session());
-//require("./passportConfig")(passport);
 
+app.use(cookieParser("secretcode"));
+app.use(passport.initialize());
+app.use(passport.session());
+require("./passportConfig")(passport);
+app.use(
+  session({
+    secret: "secretcode",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+
+app.use(cors());
 
 app.use('/', user)
 app.use('/', event)
