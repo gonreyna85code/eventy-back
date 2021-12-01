4440c5437fb15acc0a3b3c3cb92a9bd4834cb9db
gonzalo <gonreyna85@gmail.com>
2021-12-01 18:40:00 -0300

    make it better

--- server.js
+++ server.js
@@ -22,13 +22,6 @@ mongoose.connect(
     console.log("Mongoose Is Connected");
   }
 );
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-  next();
-});
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
