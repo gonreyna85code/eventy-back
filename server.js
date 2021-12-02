492fed7952672e8d0381c07bdd39f9a518c97fc9
gonzalo <gonreyna85@gmail.com>
2021-12-02 19:35:53 -0300

    make it better

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ app.use(
 );
 
 app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Credentials', '*');
   res.header('Access-Control-Allow-Origin', req.headers.origin);
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
@@ -56,7 +56,7 @@ app.use(function(req, res, next) {
    }
   });
 
-app.use(cors())
+//app.use(cors())
 
 app.use(cookieParser("secretcode"));
 app.use(passport.initialize());
