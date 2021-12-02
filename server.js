a5c4b1df24687d16d2616aeee38d19070d3f1c37
gonzalo <gonreyna85@gmail.com>
2021-12-02 01:07:36 -0300

    make it better

--- server.js
+++ server.js
@@ -29,6 +29,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(cookieParser("secretcode"));
+app.set('trust proxy', 1);
 app.use(passport.initialize());
 app.use(passport.session());
 require("./passportConfig")(passport);
