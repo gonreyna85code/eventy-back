d846b27fc987c8c6d53ce64f307c0ea82a761719
gonzalo <gonreyna85@gmail.com>
2021-12-02 19:46:48 -0300

    make it better

--- server.js
+++ server.js
@@ -44,12 +44,22 @@ app.use(
   })
 );
 
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Origin', req.headers.origin);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
+  if ('OPTIONS' == req.method) {
+       res.send(200);
+   } else {
+       next();
+   }
+  });
 
-
-app.use(cors({
-  origin: 'https://eventy-main-f73p4d16h-gonreyna85code.vercel.app',
-  credentials: true,
-}))
+  app.use(cors({
+    origin: 'https://eventy-main-f73p4d16h-gonreyna85code.vercel.app',
+    credentials: true,
+  }))
 
 app.use(cookieParser("secretcode"));
 app.use(passport.initialize());
