1ec1dff8e18ba2eba2ae3aaca25839d8967abc8a
gonzalo <gonreyna85@gmail.com>
2021-12-02 00:28:11 -0300

    make it better

--- server.js
+++ server.js
@@ -22,7 +22,21 @@ mongoose.connect(
   }
 );
 
-
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://eventy-main.vercel.app"); // update to match the domain you will make the request from
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  if (req.method === "OPTIONS") {
+    res.header(
+      "Access-Control-Allow-Methods",
+      "GET, POST, OPTIONS, PUT, DELETE"
+    );
+    return res.status(200).json({});
+  }
+  next();
+});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
