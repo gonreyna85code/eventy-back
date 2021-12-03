1900b0bed49d754092489cf4fea94a4e818a7e73
gonzalo <gonreyna85@gmail.com>
2021-12-03 12:14:28 -0300

    make it better

--- server.js
+++ server.js
@@ -53,6 +53,7 @@ app.use(
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     preflightContinue: false,
     optionsSuccessStatus: 204,
+    allowedHeaders: 'Content-Type,Authorization',
     credentials: true,
   })
 );
