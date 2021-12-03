db0351d94c9a683ef4b11e185038739477125a8e
gonzalo <gonreyna85@gmail.com>
2021-12-03 20:32:47 -0300

    xc

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(
     credentials: true,
     preflightContinue: true,
     methods: ['GET', 'POST', 'PUT', 'PATCH' , 'DELETE', 'OPTIONS'],
-    origin: true,
+    origin: 'https://eventy-main-jsgk72m78-gonreyna85code.vercel.app',
   })
 );
 
@@ -40,7 +40,7 @@ app.use(bodyParser.json({ limit: '50mb' }));
 app.use(cookieParser());
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin','true', 'https://eventy-main-jsgk72m78-gonreyna85code.vercel.app'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Origin','true', true); 
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
