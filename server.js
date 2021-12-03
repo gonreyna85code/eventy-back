5676338874033d134014ccad059e41e84def9cce
gonzalo <gonreyna85@gmail.com>
2021-12-03 20:03:02 -0300

    m

--- server.js
+++ server.js
@@ -1,16 +1,27 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
+const cookieParser = require('cookie-parser');
 const MongoStore = require("connect-mongo");
 const express = require("express");
 const passport = require("passport");
 const passportLocal = require("passport-local").Strategy;
-//const cookieParser = require("cookie-parser");
 const session = require("express-session");
 const bodyParser = require("body-parser");
 const app = express();
 const user = require("./routes/user");
 const event = require("./routes/event");
 const cors = require("cors");
+
+app.name='API'
+
+app.use(cors());
+app.use(
+  cors({
+    origin: true,
+    credentials: true,
+
+  })
+);
 
 mongoose.connect(
   process.env.MONGO,
@@ -22,28 +33,29 @@ mongoose.connect(
     console.log("Mongoose Is Connected");
   }
 ); 
-app.set('trust proxy', 1);
-//app.enable('trust proxy')
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-//app.use(cookieParser("secretcode"));
-app.use(
-  cors({
-    origin: "https://eventy-main-jsgk72m78-gonreyna85code.vercel.app",
-    credentials: true,
 
-  })
-);
+app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(cookieParser());
 
-
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+});
 
 app.use(
   session({
-    store: MongoStore.create({ mongoUrl: process.env.MONGO }),
     secret: "secretcode",
-    resave: false,
+    resave: true,
     saveUninitialized: true,
-    cookie: { maxAge: 1000 * 60 * 60 * 24, httpOnly: false, secure: false },
+    cookie: {
+      httpOnly: true,
+      secure: true,
+      sameSite: "none",
+    }
   })
 );   
 
@@ -53,6 +65,14 @@ require("./passportConfig")(passport);
 
 app.use("/", user);
 app.use("/", event);
+
+app.use((err, req, res, next) => { 
+  const status = err.status || 500;
+  const message = err.message || err;
+  console.error(err);
+  res.status(status).send(message);
+});
+
 
 app.listen(process.env.PORT, () => {
   console.log("Server Has Started on port " + process.env.PORT);
