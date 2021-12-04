b2b83eb4d6ce6de24d172731df4ac187ff981ea5
gonzalo <gonreyna85@gmail.com>
2021-12-04 14:07:46 -0300

    x

--- server.js
+++ server.js
@@ -53,6 +53,17 @@ app.use(
     },
   })
 );   
+app.all('*', function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', 'https://eventy-main-jsgk72m78-gonreyna85code.vercel.app');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
+  res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+
+  if (req.method == 'OPTIONS') {
+      res.send(200);
+  } else {
+      next();
+  }
+});
 
 app.use(passport.initialize());
 app.use(passport.session());
