5f948aa1c6adf612b60b94f0e3f5709f198833e7
gonzalo <gonreyna85@gmail.com>
2021-12-04 18:08:57 -0300

    z

--- server.js
+++ server.js
@@ -55,10 +55,6 @@ app.use(
   })
 );   
 app.all('*', function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', 'https://eventy-main-8zy1dekj7-gonreyna85code.vercel.app');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
-  res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Credentials', true);
   if (req.method == 'OPTIONS') {
       res.send(200);
   } else {
