0f3cd119eb773b916c66384a03eded783e28c496
gonzalo <gonreyna85@gmail.com>
2021-12-04 19:04:02 -0300

    x

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(
 );
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Origin', 'https://eventy-main-qmbuke3o4-gonreyna85code.vercel.app'); // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
