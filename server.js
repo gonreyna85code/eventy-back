fa349ae85cb9f3b6fc4953c0797c381dc33107a8
gonzalo <gonreyna85@gmail.com>
2021-12-03 21:00:28 -0300

    m

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
-const cookieParser = require("cookie-parser");
+const cookieParser = require('cookie-parser');
 const MongoStore = require("connect-mongo");
 const express = require("express");
 const passport = require("passport");
@@ -12,7 +12,17 @@ const user = require("./routes/user");
 const event = require("./routes/event");
 const cors = require("cors");
 
-app.name = "API";
+app.name='API'
+
+app.use(cors());
+app.use(
+  cors({
+    credentials: true,
+    preflightContinue: true,
+    methods: ['GET', 'POST', 'PUT', 'PATCH' , 'DELETE', 'OPTIONS'],
+    origin: true,
+  })
+);
 
 mongoose.connect(
   process.env.MONGO,
@@ -23,32 +33,13 @@ mongoose.connect(
   () => {
     console.log("Mongoose Is Connected");
   }
-);
+); 
 
-app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
-app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
+app.use(bodyParser.json({ limit: '50mb' }));
 app.use(cookieParser());
 
-app.use((req, res, next) => {
-  res.header(
-    "Access-Control-Allow-Origin",
-    "https://eventy-main-jsgk72m78-gonreyna85code.vercel.app"
-  );
-  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
 
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Content-type,Accept,X-Custom-Header"
-  );
-
-  if (req.method === "OPTIONS") {
-    res.header('Access-Control-Allow-Origin', req.headers.origin);
-  } else {
-    res.header('Access-Control-Allow-Origin', '*');
-  }
-
-  return next();
-});
 
 app.use(
   session({
@@ -57,12 +48,12 @@ app.use(
     resave: true,
     saveUninitialized: true,
     cookie: {
-      httpOnly: false,
+      httpOnly: true,
       secure: true,
       sameSite: "none",
-    },
+    }
   })
-);
+);   
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -71,12 +62,13 @@ require("./passportConfig")(passport);
 app.use("/", user);
 app.use("/", event);
 
-app.use((err, req, res, next) => {
+app.use((err, req, res, next) => { 
   const status = err.status || 500;
   const message = err.message || err;
   console.error(err);
   res.status(status).send(message);
 });
+
 
 app.listen(process.env.PORT, () => {
   console.log("Server Has Started on port " + process.env.PORT);
