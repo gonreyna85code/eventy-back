32732d890c62becee2ec84f1b2a80a0ab56d0b3d
gonzalo <gonreyna85@gmail.com>
2021-12-04 19:11:46 -0300

    c

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
   if (req.method == "OPTIONS") {
-    console.log('Hola');
+    console.log(req);
     res.send(200);
   } else {
     next();
