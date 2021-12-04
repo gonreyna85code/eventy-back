54630c9c6c5d247f692669de28c5e7ef9752d2cc
gonzalo <gonreyna85@gmail.com>
2021-12-04 18:41:05 -0300

    x

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
-const cookieParser = require('cookie-parser');
+const cookieParser = require("cookie-parser");
 const express = require("express");
 const passport = require("passport");
 const passportLocal = require("passport-local").Strategy;
@@ -11,30 +11,29 @@ const user = require("./routes/user");
 const event = require("./routes/event");
 const cors = require("cors");
 const MongoStore = require("connect-mongo");
-app.name='API'
-
+app.name = "API";
 
 app.use(
   cors({
     credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'PATCH' , 'DELETE', 'OPTIONS'],
-    origin: 'https://eventy-main-qmbuke3o4-gonreyna85code.vercel.app',
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    origin: "https://eventy-main-qmbuke3o4-gonreyna85code.vercel.app",
   })
 );
 
-app.use('*', function(req, res, next) {
+app.use("*", function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  if (req.method == 'OPTIONS') {
-      res.send(200);
-  
-  }
-  else {
-  next();
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.header("Access-Control-Allow-Credentials", "true");
+  if (req.method == "OPTIONS") {
+    res.send(200);
+  } else {
+    next();
   }
 });
-
-  
 
 mongoose.connect(
   process.env.MONGO,
@@ -45,30 +44,28 @@ mongoose.connect(
   () => {
     console.log("Mongoose Is Connected");
   }
-); 
+);
 
-app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
-app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
+app.use(bodyParser.json({ limit: "50mb" }));
 app.use(cookieParser());
 
-        
 app.set("trust proxy", 1);
 
 app.use(
-  session({        
+  session({
     store: MongoStore.create({
       mongoUrl: process.env.MONGO,
-      ttl: 14 * 24 * 60 * 60 // save session for 14 days
-  }),
+      ttl: 14 * 24 * 60 * 60, // save session for 14 days
+    }),
     resave: false,
     saveUninitialized: true,
-    secret: 'secretcode',
+    secret: "secretcode",
     cookie: {
       maxAge: 1000 * 60 * 60 * 24 * 7,
     },
   })
-);   
-
+);
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -77,13 +74,12 @@ require("./passportConfig")(passport);
 app.use("/", user);
 app.use("/", event);
 
-app.use((err, req, res, next) => { 
+app.use((err, req, res, next) => {
   const status = err.status || 500;
   const message = err.message || err;
   console.error(err);
   res.status(status).send(message);
 });
-
 
 app.listen(process.env.PORT, () => {
   console.log("Server Has Started on port " + process.env.PORT);
