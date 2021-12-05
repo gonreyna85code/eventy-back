8969bd5042f2151f32106adca3e72e8ff13ccc83
gonzalo <gonreyna85@gmail.com>
2021-12-04 21:19:53 -0300

    c

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.name = "API";
 
 
 app.use(cors({
-	origin: "https://eventy-main-anx5iegko-gonreyna85code.vercel.app",
+	origin: "https://eventy-main-pmbecp9ib-gonreyna85code.vercel.app",
   credentials: true,
 	methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
 	preflightContinue: false,
