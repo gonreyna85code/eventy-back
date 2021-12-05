e887bbd1e4331c74d31754d9bdd14234ca83de0a
gonzalo <gonreyna85@gmail.com>
2021-12-04 21:43:28 -0300

    x

--- routes/user.js
+++ routes/user.js
@@ -53,6 +53,7 @@ router.get("/user", async (req, res) => {
         doc.near = near;
         doc.follows = follows;
         doc.save();
+        res.cookie("user", doc);
         res.send(doc);
       }
     }).populate('follows').populate('events');
