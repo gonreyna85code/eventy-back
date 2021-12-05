606ed1f2007bc1b726f679c7544f48147f7ab3ed
gonzalo <gonreyna85@gmail.com>
2021-12-05 16:40:51 -0300

    x

--- server.js
+++ server.js
@@ -58,7 +58,7 @@ mongoose.connect(
 app.use(express.json())
 app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 app.use(bodyParser.json({ limit: "50mb" }));
-app.use(cookieParser('secretcode'));
+app.use(cookieParser());
 app.use(morgan("dev"));
 
 
