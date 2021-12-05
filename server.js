92f540c5f0563ae967036baf360f99a037c2fd71
gonzalo <gonreyna85@gmail.com>
2021-12-05 16:06:16 -0300

    x

--- server.js
+++ server.js
@@ -21,6 +21,10 @@ app.use((req, res, next) => {
   if (req.method === 'OPTIONS') {
     res.header("Access-Control-Allow-Origin", 'https://eventy-main-4wt9jlomb-gonreyna85code.vercel.app');
     res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
     return res.status(200).json({});
 }
   res.header("Access-Control-Allow-Origin", 'https://eventy-main-4wt9jlomb-gonreyna85code.vercel.app');
