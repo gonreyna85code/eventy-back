c0da245f986a08af881d22547e50b8cdbf4f91c4
gonzalo <gonreyna85@gmail.com>
2021-12-05 18:26:17 -0300

    s

--- server.js
+++ server.js
@@ -86,8 +86,8 @@ app.use(
     cookie: {     
       domain: "eventy-main-k6m7r9hk3-gonreyna85code.vercel.app",
       expires: new Date(Date.now() + 3600000), 
-      //secure: true,
-      httpOnly: false,
+      secure: true,
+      httpOnly: true,
       sameSite: 'none',
       maxAge: 14 * 24 * 60 * 60 * 1000,      
     },
