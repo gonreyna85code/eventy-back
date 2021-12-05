32c86fc6a1d0187aa49c3aa5c9fd987cd877f345
gonzalo <gonreyna85@gmail.com>
2021-12-05 15:41:46 -0300

    x

--- server.js
+++ server.js
@@ -19,7 +19,13 @@ require("./passportConfig")(passport);
 app.options('*', cors())   
 
 
-app.use(cors());
+app.use(cors(
+
+    {
+        origin: "https://eventy-main-4wt9jlomb-gonreyna85code.vercel.app",
+        credentials: true
+    }
+));
 
 
 app.use((req, res, next) => {
