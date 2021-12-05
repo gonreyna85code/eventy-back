3c4a90a3f5941ca65b7ef7832180e085420f7073
gonzalo <gonreyna85@gmail.com>
2021-12-05 01:10:22 -0300

    x

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", 'https://eventy-main-p3zc8qkpv-gonreyna85code.vercel.app');
     res.header("Access-Control-Allow-Headers", "Content-Type");
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-    res.header("Access-Control-Allow-Credentials", true);    
+    //res.header("Access-Control-Allow-Credentials", true);    
     res.sendStatus(200);
   } else {
     next();
