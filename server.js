d8cc353115fdce65d8d2371ce50d14abb61f4e4f
gonzalo <gonreyna85@gmail.com>
2021-12-05 15:48:53 -0300

    x

--- server.js
+++ server.js
@@ -23,20 +23,16 @@ app.options('*', cors())
 
 
 app.use((req, res, next) => {
-  if(req.method == "OPTIONS") {
-    res.header("Access-Control-Allow-Origin", 'https://eventy-main-4wt9jlomb-gonreyna85code.vercel.app');
-    res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers" );
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-    res.header("Access-Control-Allow-Credentials", 'true');    
-    res.sendStatus(200);
-  } else {
-    console.log(req.body)
-  res.header('Access-Control-Allow-Origin', 'https://eventy-main-4wt9jlomb-gonreyna85code.vercel.app'); // update to match the domain you will make the request from
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, x-www-form-urlencoded, Accept');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-    next();
+  res.header("Access-Control-Allow-Origin", 'https://eventy-main-4wt9jlomb-gonreyna85code.vercel.app');
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  if (req.method === 'OPTIONS') {
+      res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+      return res.status(200).json({});
   }
+  next();
 });
 
 
