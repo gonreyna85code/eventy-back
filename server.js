3c2175959881c29fa12ccfdc06f62734ae0ee275
gonzalo <gonreyna85@gmail.com>
2021-12-05 15:24:30 -0300

    x

--- server.js
+++ server.js
@@ -33,6 +33,7 @@ app.use((req, res, next) => {
     //res.header("Access-Control-Allow-Credentials", true);    
     res.sendStatus(200);
   } else {
+    console.log(req.body)
   res.header('Access-Control-Allow-Origin', 'https://eventy-main-d2b1h4ai4-gonreyna85code.vercel.app'); // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
