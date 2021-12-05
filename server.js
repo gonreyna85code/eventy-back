13a04d7f753aa42e922db18b4725c80ff454e1ee
gonzalo <gonreyna85@gmail.com>
2021-12-05 15:35:12 -0300

    x

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", 'https://eventy-main-d2b1h4ai4-gonreyna85code.vercel.app');
     res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers" );
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-    //res.header("Access-Control-Allow-Credentials", true);    
+    res.header("Access-Control-Allow-Credentials", true);    
     res.sendStatus(200);
   } else {
     console.log(req.body)
