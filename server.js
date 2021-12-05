912c7c54acae3ce3efa6fb5398c355765a55b1ad
gonzalo <gonreyna85@gmail.com>
2021-12-05 00:28:01 -0300

    c

--- server.js
+++ server.js
@@ -23,12 +23,10 @@ app.use(cors({
 
 app.use((req, res, next) => {
   if(req.method == "OPTIONS") {
-    res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
     res.header("Access-Control-Allow-Credentials", true);
     res.send(200);
-    console.log(req.body)
   } else {
     next();
   }
