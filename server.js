24c8650226560ebaa6fed9d8827ab1f57073ff9a
gonzalo <gonreyna85@gmail.com>
2021-12-05 00:17:32 -0300

    s

--- server.js
+++ server.js
@@ -21,6 +21,23 @@ app.use(cors({
   credentials: true,
 }));
 
+app.use((req, res, next) => {
+  if(req.method == "OPTIONS") {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
+    res.header("Access-Control-Allow-Credentials", true);
+    res.send(200);
+  } else {
+    next();
+  }
+});
+
+
+
+
+
+
 
 mongoose.connect(
   process.env.MONGO,
