32ea299719f2417e1bdf9150893fb97cfde89547
gonzalo <gonreyna85@gmail.com>
2021-12-06 12:59:18 -0300

    x

--- server.js
+++ server.js
@@ -67,7 +67,7 @@ mongoose.connect(
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 app.use(bodyParser.json({ limit: "50mb" }));
-//app.use(cookieParser("secretcode"));
+app.use(cookieParser("secretcode"));
 app.use(morgan("dev"));
 
 app.use(
@@ -78,14 +78,14 @@ app.use(
     }),
     name: "admin_session",
     expires: new Date(Date.now() + 900000),
-    resave: true,
+    resave: false,
     //rolling: false,
     saveUninitialized: false,
     //unset: "destroy",
     secret: "secretcode",
     cookie: {     
       //domain: "eventy-main-k6m7r9hk3-gonreyna85code.vercel.app",
-      expires: new Date(Date.now() + 3600000), 
+      //expires: new Date(Date.now() + 3600000), 
       secure: false,
       httpOnly: true,
       sameSite: 'none',
