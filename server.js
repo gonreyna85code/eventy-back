e92296186ab0747d1ec247ddff5e2710e2a9b9fb
gonzalo <gonreyna85@gmail.com>
2021-12-06 13:02:07 -0300

    x

--- server.js
+++ server.js
@@ -89,7 +89,7 @@ app.use(
       secure: false,
       httpOnly: true,
       sameSite: 'none',
-      maxAge: 14 * 24 * 60 * 60 * 1000,      
+      //maxAge: 14 * 24 * 60 * 60 * 1000,      
     },
     
   })
