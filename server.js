f48297c2ad1939b908d2b6f1dd4cc5b6778549c2
gonzalo <gonreyna85@gmail.com>
2021-12-07 14:52:16 -0300

    z

--- server.js
+++ server.js
@@ -24,8 +24,9 @@ app.use(async (req, res, next) => {
     res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, application/x-www-form-urlencoded, Accept, Authorization, Set-Cookie, Cookie");
     return res.status(200).json({});
-    
+
   }
+  
   res.header("Access-Control-Allow-Origin", "https://eventy-main.vercel.app");
   res.header("Access-Control-Allow-Credentials", "true");
   res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
