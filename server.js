07d34c082d9f9f260c8c842471c617f54628d3da
gonzalo <gonreyna85@gmail.com>
2021-12-07 15:13:07 -0300

    x

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const app = express();
 
 app.name = "API";
 
-//app.use(  cors({origin: "https://eventy-main.vercel.app", credentials: true}));
+app.use(  cors({origin: "https://eventy-main.vercel.app", credentials: true}));
 
 app.set("trust proxy", 1);
 
