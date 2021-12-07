3e2c16d9866b3593146eeb153e2a5b651a0ce399
gonzalo <gonreyna85@gmail.com>
2021-12-06 21:18:01 -0300

    x

--- server.js
+++ server.js
@@ -15,88 +15,33 @@ const app = express();
 
 app.name = "API";
 
-app.use(
-  cors({
-    origin: "https://eventy-main-k9u23v0l7-gonreyna85code.vercel.app",
-    credentials: true
-
-  })
-);
+app.use(  cors({origin: "https://eventy-main-k9u23v0l7-gonreyna85code.vercel.app", credentials: true}));
 
 app.set("trust proxy", 1);
 
 app.use(async (req, res, next) => {
-  console.log("req.headers", req.headers);
-  console.log("req.cookies", req.cookies);
   if (req.method === "OPTIONS") {
-    res.header(
-      "Access-Control-Allow-Origin",
-      "https://eventy-main-k9u23v0l7-gonreyna85code.vercel.app"
-    );
+    res.header("Access-Control-Allow-Origin", "https://eventy-main-k9u23v0l7-gonreyna85code.vercel.app");
     res.header("Access-Control-Allow-Credentials", "true");
     res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, application/x-www-form-urlencoded, Accept, Authorization, Set-Cookie, Cookie"
-    );
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, application/x-www-form-urlencoded, Accept, Authorization, Set-Cookie, Cookie");
     return res.status(200).json({});
   }
-  res.header(
-    "Access-Control-Allow-Origin",
-    "https://eventy-main-k9u23v0l7-gonreyna85code.vercel.app"
-  );
+  res.header("Access-Control-Allow-Origin", "https://eventy-main-k9u23v0l7-gonreyna85code.vercel.app");
   res.header("Access-Control-Allow-Credentials", "true");
   res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, application/x-www-form-urlencoded, Cookie, Accept, Authorization, Set-Cookie"
-  );
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, application/x-www-form-urlencoded, Cookie, Accept, Authorization, Set-Cookie");
   next();
 });
 
 mongoose.connect(
-  process.env.MONGO,
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  },
-  () => {
-    console.log("Mongoose Is Connected");
-  }
-);
+  process.env.MONGO,{useNewUrlParser: true,useUnifiedTopology: true},
+  () => {console.log("Mongoose Is Connected");});
 mongoose.set("useCreateIndex", true);
 
 app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 app.use(bodyParser.json({ limit: "50mb" }));
 app.use(morgan("dev"));
-
-const store = MongoStore.create({
-  mongoUrl: process.env.MONGO,
-  ttl: 14 * 24 * 60 * 60,
-});
-
-app.use(
-  session({
-    name:session.name,
-    secret: "mysecret",
-    resave: true,
-    saveUninitialized: true,
-    store: store,
-    keys: {
-      public:
-        "-----BEGIN PUBLIC KEY-----\nMFYwEAYHKoZIzj0CAQYFK4EEAAoDQgAEDXMuNS4pyqkpZwij+UCcTPVStZHmG39D\nP1V7qaPCfc0ewXXbcEaJiarqjHOM5a6SVivCaUdJj+25tjMk4sPchQ==\n-----END PUBLIC KEY-----",
-      private:
-        "-----BEGIN PRIVATE KEY-----\nMIGEAgEAMBAGByqGSM49AgEGBSuBBAAKBG0wawIBAQQgvK1dk5M81nax8lQxpbWo\nsB1oK9YAqRP7MwWc7wDne8ehRANCAAQNcy41LinKqSlnCKP5QJxM9VK1keYbf0M/\nVXupo8J9zR7BddtwRomJquqMc4zlrpJWK8JpR0mP7bm2MyTiw9yF\n-----END PRIVATE KEY-----",
-    },
-    cookie: {
-      //domain: "https://eventy-main-6hcqxvt4w-gonreyna85code.vercel.app",
-      httpOnly: true,
-      secure: false,
-      maxAge: 1000 * 60 * 60 * 24 * 14,
-      sameSite: "none",
-    },
-  })
-);
 
 require("./passportConfig")(passport);
 app.use(passport.initialize());
