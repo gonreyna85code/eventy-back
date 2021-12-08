e6e2d843c7403c55e6b6e14cfb4cacac897de084
gonzalo <gonreyna85@gmail.com>
2021-12-08 14:48:42 -0300

    x

--- passportConfig.js
+++ passportConfig.js
@@ -22,6 +22,18 @@ module.exports = function (passport) {
       });
     })
   ); 
+
+  passport.use(new GoogleStrategy({
+    consumerKey: GOOGLE_CONSUMER_KEY,
+    consumerSecret: GOOGLE_CONSUMER_SECRET,
+    callbackURL: "http://www.example.com/auth/google/callback"
+  },
+  function(token, tokenSecret, profile, done) {
+      User.findOrCreate({ googleId: profile.id }, function (err, user) {
+        return done(err, user);
+      });
+  }
+  ));
   
   passport.use(         
     new JWTstrategy(
@@ -41,14 +53,3 @@ module.exports = function (passport) {
   );  
 };
 
-passport.use(new GoogleStrategy({
-  consumerKey: GOOGLE_CONSUMER_KEY,
-  consumerSecret: GOOGLE_CONSUMER_SECRET,
-  callbackURL: "http://www.example.com/auth/google/callback"
-},
-function(token, tokenSecret, profile, done) {
-    User.findOrCreate({ googleId: profile.id }, function (err, user) {
-      return done(err, user);
-    });
-}
-));
