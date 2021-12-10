bdf7c86b9985bd8bcfcde5baae4556d2402a708a
gonzalo <gonreyna85@gmail.com>
2021-12-10 16:00:53 -0300

    x

--- passportConfig.js
+++ passportConfig.js
@@ -52,7 +52,7 @@ module.exports = function (passport) {
       },
       function(accessToken, refreshToken, profile, done) {
         console.log("profile :",profile.email);
-        User.findOne({ profile: {email: profile.email }}, function (err, user) {
+        User.findOne({ profile: {'email': profile.email }}, function (err, user) {
           console.log("user :",user);
           return done(err, user);
         });
